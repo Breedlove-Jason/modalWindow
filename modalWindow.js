63d2b445a173f8ac3b1bc918725ec5be44e4d23e
Jason Breedlove <jason@jasonbreedlove.com>
2022-04-17 19:24:30 -0700

    complete feature

---
+++ modalWindow.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnOpenModal = document.querySelectorAll(".show-modal");
+
+const openModal = function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = function(){
+    overlay.classList.add('hidden');
+    modal.classList.add('hidden')
+};
+for (let i = 0; i < btnOpenModal.length; i++){
+    btnOpenModal[i].addEventListener('click', openModal);
+        console.log('Button Clicked');
+    }
+
+    btnCloseModal.addEventListener('click', closeModal);
+    overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function(event){
+   console.log(event);
+   if(event.key === 'Escape' && !modal.classList.contains('hidden')){
+           closeModal();
+       }
+
+});
+
